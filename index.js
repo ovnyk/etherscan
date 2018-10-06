ded7beb2ba9d0bc42b4709eb038baf50326864e9
Anastasiia <ovnykca@gmail.com>
2018-10-06 14:35:23 -0400

    start

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("nosdfhs")
+
+
+var ethertotal = document.querySelector("#ethertotal");
+console.log(ethertotal)
+
+window.addEventListener("load", () => {
+    console.log("CSDUFG")
+    var ethertotal = document.querySelector("#ethertotal");
+    console.log(ethertotal)
+
+}) 
+
+
+
+
+
+
+console.log("gogol")
