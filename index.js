eab9a16af0bc83ddfd9c1e21baa60c805f6e1595
Anastasiia <ovnykca@gmail.com>
2018-10-07 23:57:02 -0400

    end

--- index.js
+++ index.js
@@ -126,17 +126,19 @@ window.addEventListener("load", () => {
     })
 }) 
 
-const URLtransaction = "https://api.etherscan.io/api?module=proxy&action=eth_getTransactionCount&address=0x2910543af39aba0cd09dbb2d50200b3e800a63d2&tag=latest&apikey="
 
-const FULL_URL4 = URLtransaction + TOKEN
+const URLblocks = "https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=0x10d4f&boolean=true&apikey="
+
+const FULL_URL5 = URLblocks + TOKEN
 
 window.addEventListener("load", () => {
-    fetch(FULL_URL4)
+    fetch(FULL_URL5)
     .then(function(result){
         var value = result.json()
         value.then(function(val){
-            var ethertotal = document.querySelector("#transaction");
-            ethertotal.innerHTML += " " + parseInt(val.result, 16)
+            var ethertotal = document.querySelector("#blocks");
+            ethertotal.innerHTML += " " + parseInt(val.result.gasLimit, 16)
+            console.log(val.result)
         }) 
     })
     .then(function(res) {
